46e2af9f6a3404f80f3fc490f80c37b080382fb2
808bass <2798848176@qq.com>
2024-02-17 11:46:33 +0800

    add MinecraftButtonGray

--- app/src/main/java/com/treatangus/joyya/MainActivity.java
+++ app/src/main/java/com/treatangus/joyya/MainActivity.java
@@ -1,42 +1,60 @@
 package com.treatangus.joyya;
 
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
 
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
+import androidx.appcompat.app.AppCompatActivity;
 import com.treatangus.joyya.databinding.ActivityMainBinding;
-import com.treatangus.joyya.mcgui.MinecraftButton;
+import com.treatangus.joyya.mcgui.MinecraftButtonGreen;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 
+import java.util.Timer;
+import java.util.TimerTask;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
-    private MinecraftButton start;
+    private MinecraftButtonGreen start;
+    private Timer mTimer;
+    private TimerTask systemUiTimerTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //隐藏系统状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        initUI();
+        //窗口
+        getWindow().getDecorView().post(new Runnable() {
+            @Override
+            public void run() {
+                hideSystemUI(getWindow().getDecorView());
+                mTimer = new Timer();
+            }
+        });
 
+    }
+    private void initUI() {
         setSupportActionBar(binding.toolbar);
+        start = binding.start;
+    }
 
-       start = binding.start;
-
+        @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            View decorView = getWindow().getDecorView();
+            decorView.setOnSystemUiVisibilityChangeListener(null);
+            hideSystemUI(decorView);
+        } else {
+            View decorView = getWindow().getDecorView();
+            decorView.setOnSystemUiVisibilityChangeListener(null);
+            if (systemUiTimerTask != null) systemUiTimerTask.cancel();
+        }
     }
 
     @Override
@@ -60,4 +78,16 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    private void hideSystemUI(View decorView) {
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE
+                // Set the content to appear under the system bars so that the
+                // content doesn't resize when the system bars hide and show.
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
 }
--- app/src/main/java/com/treatangus/joyya/mcgui/MinecraftButtonGray.java
+++ app/src/main/java/com/treatangus/joyya/mcgui/MinecraftButtonGray.java
@@ -14,25 +14,24 @@ import android.view.*;
 
 import java.io.IOException;
 
-public class MinecraftButton extends androidx.appcompat.widget.AppCompatButton
-{
-    private ColorDrawable left = new ColorDrawable(Color.parseColor("#4f9443"));
-    private ColorDrawable top = new ColorDrawable(Color.parseColor("#1c4e13"));
-    private ColorDrawable stroke = new ColorDrawable(Color.parseColor("#FF000000"));
-    private ColorDrawable right = new ColorDrawable(Color.parseColor("#5e9f4d"));
-    private ColorDrawable bottom = new ColorDrawable(Color.parseColor("#50913f"));
-    private ColorDrawable topStroke = new ColorDrawable(Color.parseColor("#61a153"));
-    private ColorDrawable bgNormal = new ColorDrawable(Color.parseColor("#3b8526"));
+public class MinecraftButtonGray extends androidx.appcompat.widget.AppCompatButton {
+    private ColorDrawable left = new ColorDrawable(Color.parseColor("#ecedee"));
+    private ColorDrawable top = new ColorDrawable(Color.parseColor("#ecedee"));
+    private ColorDrawable stroke = new ColorDrawable(Color.parseColor("#1e1e1e"));
+    private ColorDrawable right = new ColorDrawable(Color.parseColor("#e3e3e5"));
+    private ColorDrawable bottom = new ColorDrawable(Color.parseColor("#e3e3e5"));
+    private ColorDrawable topStroke = new ColorDrawable(Color.parseColor("#ecedee"));
+    private ColorDrawable bgNormal = new ColorDrawable(Color.parseColor("#d0d1d4"));
 
-    private ColorDrawable strokeFocus = new ColorDrawable(Color.parseColor("#1e1c21"));
-    private ColorDrawable leftFocus = new ColorDrawable(Color.parseColor("#4b6e46"));
-    private ColorDrawable topFocus = new ColorDrawable(Color.parseColor("#4b7243"));
-    private ColorDrawable rightFocus = new ColorDrawable(Color.parseColor("#3b6132"));
-    private ColorDrawable bottomFocus = new ColorDrawable(Color.parseColor("#375c31"));
-    private ColorDrawable bgFocus = new ColorDrawable(Color.parseColor("#1d4d13"));
+    private ColorDrawable strokeFocus = new ColorDrawable(Color.parseColor("#1e1e1e"));
+    private ColorDrawable leftFocus = new ColorDrawable(Color.parseColor("#e0e0e1"));
+    private ColorDrawable topFocus = new ColorDrawable(Color.parseColor("#e0e0e1"));
+    private ColorDrawable rightFocus = new ColorDrawable(Color.parseColor("#d0d1d3"));
+    private ColorDrawable bottomFocus = new ColorDrawable(Color.parseColor("#d0d1d3"));
+    private ColorDrawable bgFocus = new ColorDrawable(Color.parseColor("#b1b2b5"));
     private AssetManager assetManager;
     private SoundPool MinecraftButtonSound;
-    private  AssetFileDescriptor fileDescriptor;
+    private AssetFileDescriptor fileDescriptor;
     private int soundId;
     private int streamId;
     private MediaPlayer MinecraftButtonSound2;
@@ -59,15 +58,16 @@ public class MinecraftButton extends androidx.appcompat.widget.AppCompatButton
 
     private LayerDrawable layerdrawable, layerdrawablefocus;
 
-    public MinecraftButton(Context ctx) {
+    public MinecraftButtonGray(Context ctx) {
         this(ctx, null);
     }
 
-    public MinecraftButton(Context ctx, AttributeSet attrs) {
+    public MinecraftButtonGray(Context ctx, AttributeSet attrs) {
         super(ctx, attrs);
         initButtonSound(); //因为SoundPool需要预加载声音资源，否则会出现第一次按下按钮没有声音的问题
         init();
     }
+
     public void init() {
         getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
@@ -76,21 +76,21 @@ public class MinecraftButton extends androidx.appcompat.widget.AppCompatButton
                 layerdrawable.setLayerInset(0, 0, 0, 0, 0); // stroke
                 layerdrawable.setLayerInset(1, 4, 4, 4, 4); // top
                 layerdrawable.setLayerInset(2, 4, 8, 4, getHeight() - 4); // topStroke
-                layerdrawable.setLayerInset(3, 4, 8, getWidth() - 12,16); // left
+                layerdrawable.setLayerInset(3, 4, 8, getWidth() - 12, 16); // left
                 layerdrawable.setLayerInset(4, getWidth() - 12, 8, 4, 16); // right
-                layerdrawable.setLayerInset(5, 4, getHeight() - 12,4,16);// bottom
+                layerdrawable.setLayerInset(5, 4, getHeight() - 12, 4, 16);// bottom
                 layerdrawable.setLayerInset(6, 8, 8, 8, 16); // bg
 
                 layerdrawablefocus = new LayerDrawable(DrawableArrayFocus);
                 layerdrawablefocus.setLayerInset(0, 0, 0, 0, 0); // stroke
                 layerdrawablefocus.setLayerInset(1, 4, 4, 4, 4); // top
-                layerdrawablefocus.setLayerInset(2, 4, 4, getWidth() - 4,4); // left
+                layerdrawablefocus.setLayerInset(2, 4, 4, getWidth() - 4, 4); // left
                 layerdrawablefocus.setLayerInset(3, getWidth() - 4, 4, 4, 4); // right
-                layerdrawablefocus.setLayerInset(4, 4, getHeight() - 4,4,4);// bottom
+                layerdrawablefocus.setLayerInset(4, 4, getHeight() - 4, 4, 4);// bottom
                 layerdrawablefocus.setLayerInset(5, 8, 8, 8, 8); // bg
 
                 setBackgroundDrawable(layerdrawable);
-                setTextColor(Color.WHITE);
+                setTextColor(Color.BLACK);
                 setPadding(10, 10, 10, 10);
                 //setOnTouchListener(null);
             }
@@ -101,15 +101,13 @@ public class MinecraftButton extends androidx.appcompat.widget.AppCompatButton
 
 
     @Override
-    public void setEnabled(boolean enabled)
-    {
+    public void setEnabled(boolean enabled) {
         super.setEnabled(enabled);
         setBackgroundDrawable(enabled ? layerdrawable : layerdrawablefocus);
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event)
-    {
+    public boolean onTouchEvent(MotionEvent event) {
         if ((event.getAction() == event.ACTION_UP) && !isUp && isEnabled()) {
             setBackgroundDrawable(layerdrawable);
             setTranslationY(0f); //松手时还原
@@ -140,7 +138,7 @@ public class MinecraftButton extends androidx.appcompat.widget.AppCompatButton
                 MinecraftButtonSound = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
             }
             assetManager = getContext().getAssets();
-            fileDescriptor = assetManager.openFd("sounds/Minecraft_Button_Green_Sound.ogg");
+            fileDescriptor = assetManager.openFd("sounds/Minecraft_Button_Gray_Sound.ogg");
             soundId = MinecraftButtonSound.load(fileDescriptor, 1);
         } catch (IOException e) {
             e.printStackTrace();
@@ -155,28 +153,4 @@ public class MinecraftButton extends androidx.appcompat.widget.AppCompatButton
         MinecraftButtonSound2.release();
         super.onDetachedFromWindow();
     }
-
-   /* private void MediaPlayer用法() {
-        try {
-            if (MinecraftButtonSound2 == null) {
-
-                assetManager = getContext().getAssets();
-                AssetFileDescriptor fileDescriptor = assetManager.openFd("sounds/Minecraft_Button_Green_Sound.ogg");
-                MinecraftButtonSound2 = new MediaPlayer();
-                MinecraftButtonSound2.setDataSource(fileDescriptor.getFileDescriptor(),
-                        fileDescriptor.getStartOffset(),
-                        fileDescriptor.getLength());
-                MinecraftButtonSound2.prepare();
-                MinecraftButtonSound2.start();
-            } else if (MinecraftButtonSound2.isPlaying()) {
-                MinecraftButtonSound2.stop();
-                MinecraftButtonSound2.prepare();
-                MinecraftButtonSound2.start();
-            } else {
-                MinecraftButtonSound2.start();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    } */
 }
